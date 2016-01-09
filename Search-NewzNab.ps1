a8dac6a4579602afbe23e3379189122df79d1800
Ben Gould <bgould@worldremit.com>
2016-01-09 00:18:57 +0000

    placeholders

---
+++ Search-NewzNab.ps1
@@ -0,0 +1,21 @@
+﻿# Module for newznab searching and downloading
+
+function Search-Newznab
+    {
+    [OutputType([Object])]
+    Param
+        (
+        # API URL of NewzNab
+        [String]$NewzNab,
+
+        # API key of NewzNab
+        [String]$APIKey,
+    
+        #String to search for
+
+        [string]$searchString
+        )
+    
+    
+    }
+
---
+++ Send-Download.ps1
@@ -0,0 +1,31 @@
+﻿# Module for newznab searching and downloading
+
+function Send-Download
+    {
+    [OutputType([bool])]
+    Param
+        (
+        # URL of SabNZBd+
+        [String]
+        $SabNZBdplus,
+
+        # API key of SabNZBd+
+        [String]
+        $APIKey,
+    
+        # URL of NZB
+        [Parameter(Mandatory=$True)]
+        [string]
+        $NZBURL,
+
+        # Category to place download in
+        [Parameter(Mandatory=$True)]
+        [validateSet("tv","movies","music")]
+        [string]
+        $sabCategory
+
+        )
+    
+    
+    }
+
