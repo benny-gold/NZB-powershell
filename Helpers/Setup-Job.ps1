edae6f59b6eac9e0e17f0463d45f1dd786f2c12f
Ben Gould <ben@bengould.co.uk>
2016-01-16 23:44:30 +0000

    Job Scheduler

---
+++ Helpers/Setup-Job.ps1
@@ -0,0 +1,11 @@
+﻿Param
+(
+[Parameter(Mandatory=$True,Position=1)]
+[string]$ItemToSearchFor
+)
+$JobTrigger = New-JobTrigger -At $((get-date).AddSeconds(30)) -RepeatIndefinitely -RepetitionInterval (New-TimeSpan -Minutes 15) -Once
+$JobOption = New-ScheduledJobOption -RequireNetwork -RestartOnIdleResume -StartIfIdle
+Register-ScheduledJob -Name "Powershell Downloader - $ItemToSearchFor" -ScriptBlock `
+    {C:\Git\Repos\NZB-powershell\Get-NewDownload.ps1
+    Get-NewDownload -searchString $ItemToSearchFor -GetLatest -maxSize 500
+    } -Trigger $JobTrigger -ScheduledJobOption $JobOption
