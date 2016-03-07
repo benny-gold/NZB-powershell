4e6badb9be21350ff06eb32a890d588948733be7
Ben Gould <ben@bengould.co.uk>
2016-03-07 23:06:26 +0000

    basic calendar pull

--- Get-SonarrCalendar.ps1
+++ Get-SonarrCalendar.ps1
@@ -1,3 +1,25 @@
 ﻿function Get-SonarrCalendar {
+    Param
+        (
+        [Parameter(Mandatory=$True,Position=0)]
+        [string]
+        $sonarrURL,
 
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]
+        $sonarrAPIKey
+               
+        )
+
+        $headers = @{
+            "X-Api-Key"=$sonarrAPIKey
+            }
+
+        $apiCall = "$sonarrURL/api/Calendar"
+        
+        write-verbose $apiCall 
+        $Calendar = Invoke-RestMethod -Method Get -Uri $apiCall -Headers $headers
+        return $Calendar
 }
+
+
--- tests/Get-SonarrCalendar.Tests.ps1
+++ tests/Get-SonarrCalendar.Tests.ps1
@@ -3,7 +3,8 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\..\$sut"
 
 Describe "Get-SonarrCalendar" {
-    It "does something useful" {
-        $true | Should Be $false
+    It "Should return tomorrow's shows." {
+        $tomorrow = (Get-Date).AddDays(1).ToString("yyyy-MM-dd")
+       (Get-SonarrCalendar -sonarrURL $SonarrURL -sonarrAPIKey $SonarrKey).airDate | Should Be $tomorrow
     }
 }
