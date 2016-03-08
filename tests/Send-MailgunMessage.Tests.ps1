0de71f9988e7a5f009304452ba83f1eede55f7f6
Ben Gould <ben@bengould.co.uk>
2016-03-08 16:25:58 +0000

    email test

---
+++ tests/Send-MailgunMessage.Tests.ps1
@@ -0,0 +1,14 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\..\Notifications\$sut"
+. "$here\..\secrets.ps1"
+
+
+Describe "Send-MailgunMessage" {
+    It "Sends Test Email" {
+        $successMessage =  "Queued. Thank you."
+        (Send-MailgunMessage -to $mailRecipient1 -fromName UnitTest -sender "Unit Test" -Domain $mailDomain -APIKey $mailgunKey -html "This is a Unit Test").Message | Should Be $successMessage
+    }
+
+}
+
