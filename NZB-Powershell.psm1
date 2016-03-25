32f07e8a5dc744ed9b7dd16c341b85908aaee266
Ben Gould <ben@bengould.co.uk>
2016-03-25 14:14:03 +0000

    new URL encoder

--- NZB-Powershell.psm1
+++ NZB-Powershell.psm1
@@ -3,6 +3,7 @@ Gci "$PSScriptRoot\Notifications\*.ps1" | ForEach-Object {. $_.FullName}
 Gci "$PSScriptRoot\Providers\*.ps1" | ForEach-Object {. $_.FullName}
 Gci "$PSScriptRoot\Sonarr\*.ps1" | ForEach-Object {. $_.FullName}
 Gci "$PSScriptRoot\SabNZBd+\*.ps1" | ForEach-Object {. $_.FullName}
+. "$PSScriptRoot\Helpers\Invoke-URLEncoding.ps1"
 . "$PSScriptRoot\secrets.ps1"
 . "$PSScriptRoot\Get-NewDownload.ps1"
 
---
+++ tests/Invoke-URLEncoding.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+Import-Module "$here\..\NZB-Powershell" -Force
+
+Describe "Invoke-URLEncoding" {
+    It "Encodes &" {
+        $true | Should Be $false
+    }
+}
