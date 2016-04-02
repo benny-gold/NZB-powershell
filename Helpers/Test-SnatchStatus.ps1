4bbb6ecee789b424281056a494a0ced239b3b041
Ben Gould <ben@bengould.co.uk>
2016-04-02 18:04:30 +0100

    wrote tests

--- Helpers/Test-SnatchStatus.ps1
+++ Helpers/Test-SnatchStatus.ps1
@@ -1,3 +1,29 @@
 ﻿function Test-SnatchStatus {
+    [OutputType([bool])]
+    Param
+        (
+        [Parameter(Mandatory=$True,Position=0)]
+        [string]$guid,
+
+        [Parameter(Mandatory=$True,Position=1)]
+        [string]$documentDBLocation
+
+
+        )
+    
+    if(!(Test-Path $documentDBLocation)) {
+        md $documentDBLocation
+        }
+    
+    $documentLocation = "$documentDBLocation\$($SelectedDownload.guid).json"
+
+    if(!(Test-Path $documentLocation))
+        {
+        return $false
+        }
+    else
+        {
+        return $True
+        }
 
 }
--- tests/Test-SnatchStatus.Tests.ps1
+++ tests/Test-SnatchStatus.Tests.ps1
@@ -1,9 +1,10 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-. "$here\$sut"
+Import-Module "$here\..\NZB-Powershell" -Force
+
 
 Describe "Test-SnatchStatus" {
-    It "does something useful" {
-        $true | Should Be $false
+    It "Returns false if file doesn't exist" {
+        Test-SnatchStatus -guid sj789yt3kjbd80fgs -documentDBLocation $documentDBLocation | Should Be $false
     }
 }
