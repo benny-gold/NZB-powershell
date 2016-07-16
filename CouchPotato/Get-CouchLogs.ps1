e1bc6a669f1b743eddadd7afba594280a70f0e35
Ben Gould <ben@bengould.co.uk>
2016-07-16 17:06:17 +0100

    Get logs #14

--- CouchPotato/Get-CouchLogs.ps1
+++ CouchPotato/Get-CouchLogs.ps1
@@ -8,14 +8,46 @@
 
         [Parameter(Mandatory=$True,Position=1)]
         [string]
-        $couchApiKey
+        $couchApiKey,
+
+        # 	Number of lines. Last to first. Default 30
+        [Parameter(Mandatory=$false,Position=2)]
+        [int]
+        $lines=30,
+
+        [Parameter(Mandatory=$false,Position=3)]
+        [ValidateSet("all","error","info","debug")]
+        [string]
+        $logType="all"
         )
 
         # Couchpotato's renamer doesn't work. This function forces a restart after each item is moved. It stops when the "From" path is empty.
 
+        class couchLog{ 
+            [datetime]$logDate
+            [string]$type
+            [string]$logCategory
+            [string]$logMessage
+            }
 
         $loggingEndpoint = "logging.partial"
 
-        $logs = Invoke-RestMethod "$CouchURL/api/$couchApiKey/$($loggingEndpoint)"
-        $logs.log
+        $logs = Invoke-RestMethod "$CouchURL/api/$couchApiKey/$($loggingEndpoint)?lines=$lines&type=$logType"
+        $logObjectArray = @()
+
+        foreach($logEntry in $logs.log)
+            {
+            $logObject = New-Object -TypeName couchLog
+            $logObject.logCategory = $logEntry.message -replace '(\[(?:\w+\.)+\w+\])(\s)([\s\S]+)',"`$1"
+            $logObject.logMessage = $logEntry.message -replace '(\[(?:\w+\.)+\w+\])(\s)([\s\S]+)',"`$3"
+
+            $logObject.logDate = [datetime]::ParseExact($($logEntry.time), "MM-dd HH:mm:ss", [System.Globalization.CultureInfo]::InvariantCulture, [System.Globalization.DateTimeStyles]::None)
+
+            $logObject.type = $logEntry.type
+            $logObjectArray += $logObject
+            }
+
+
+        return $logObjectArray
 }
+
--- tests/Get-CouchLogs.Tests.ps1
+++ tests/Get-CouchLogs.Tests.ps1
@@ -5,9 +5,15 @@ Import-Module "$here\..\NZB-Powershell" -Force
 
 Describe "Get-CouchLogs" {
     It "Should return log entries" {
-        $true | Should Be $false
+        (Get-CouchLogs -couchURL $CouchURL -couchApiKey $couchKey) | Should not be $null
     }
     It "Should change entries into objects" {
-        $true | Should Be $false
+        ((Get-CouchLogs -couchURL $CouchURL -couchApiKey $couchKey -lines 7)[0] | Get-Member | Where-Object {$_.MemberType -like "Property"}).count | Should Be 4
+    }
+    It "Should return the correct number of entries" {
+        (Get-CouchLogs -couchURL $CouchURL -couchApiKey $couchKey -lines 7).count | Should Be 7
+    }
+    It "Should return the correct type of entries" {
+        (Get-CouchLogs -couchURL $CouchURL -couchApiKey $couchKey -logtype ERROR)[0].type | Should match "ERROR"
     }
 }
