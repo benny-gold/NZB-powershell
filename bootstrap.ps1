6459d3ec1d0b4d20a1c3b22205fc049f99851313
Ben Gould <ben@bengould.co.uk>
2016-07-31 19:16:43 +0100

    test for empty value

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,13 @@
 Get-Content ./secretstemplate.txt | % {iex $_}
 . ./Create-SettingsObject.ps1 | ConvertTo-Json -Depth 4 | Out-File -FilePath ./AdewaleSettings.config
+
+if ($AdewaleSettings.SabSettings.sabKey -eq $null) {
+    Write-Warning "Please ensure you have filled out all the settings in secretstemplate.txt"
+    }
+
+
 $env:AdewaleSettingsPath = "$PSScriptRoot\AdewaleSettings.config"
+
 
 if (test-path $env:AdewaleSettingsPath) {
     Write-Host -foregroundColor Green "Adewale Settings configured Successfully."
